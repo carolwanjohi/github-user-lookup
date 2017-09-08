e856594a5c8ff58e3283036890f1385d0c661b72
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-09-08 17:08:59 +0300

    Add repo info Add repo name and description to a githubuser's name

--- js/lookup.js
+++ js/lookup.js
@@ -10,16 +10,37 @@ Repos.prototype.getRepos = function(userInfo, displayFunction){
   $.get('https://api.github.com/users/' + userInfo + 
     '?access_token=' + apiKey
     ).then(function(response){
-        // Debugger
-        console.log(response.name);
 
         // Display's user's info
         displayFunction(response.name);
+
     }).fail(function(error){
-        // Debugger
-        console.log(error.responseJSON.message);
-        
+        // Display error message when an error is encountered
         $('#showUserInfo').append(error.responseJSON.message);
+    });
+
+  $.get('https://api.github.com/users/' + userInfo + 
+    '/repos?access_token=' + apiKey).then(function(response){
+
+
+        for (var index = 0; index <= response.length; index++ ) {
+
+            // Check if a repo has a description and display
+            if( response[index].description === null) {
+                $('#showRepoInfo').append("<li> <h5>Repo name: " + response[index].name + ". </h5> <p> This repo has no description </p> </li>");
+            } else {
+
+                $('#showRepoInfo').append("<li> <h5>Repo name: " + response[index].name + ". </h5> <p>" + response[index].description + "</p> </li>"); 
+            }
+        }
+
+    }).fail(function(error){
+        // Currently producing an error on the console, to be fixed
+
+        // Debugger
+        // console.log(error.responseJSON.message);
+        
+        // $('#showUserInfo').append(error.responseJSON.message);
     });
 };
 
