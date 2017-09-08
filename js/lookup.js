ea75511bbfb21a625fb804b648728ec017770480
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-09-08 13:07:39 +0300

    Add display function Add displayFunction as a parameter of getRepos function to display the name Display 404 Not Found error

--- js/lookup.js
+++ js/lookup.js
@@ -3,16 +3,23 @@ var apiKey = require('./../.env').apiKey;
 
 function Repos() {}
 
-Repos.prototype.getRepos = function(userInfo){
+Repos.prototype.getRepos = function(userInfo, displayFunction){
     // Debugger  
     console.log('Are you ready to look-up');
 
   $.get('https://api.github.com/users/' + userInfo + 
     '?access_token=' + apiKey
     ).then(function(response){
-        console.log(response);
+        // Debugger
+        console.log(response.name);
+
+        // Display's user's info
+        displayFunction(response.name);
     }).fail(function(error){
+        // Debugger
         console.log(error.responseJSON.message);
+        
+        $('#showUserInfo').append(error.responseJSON.message);
     });
 };
 
