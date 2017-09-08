e3d2d74bc49806884105312224dd9389dc9b3b0e
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-09-08 11:13:48 +0300

    Create front end js Create lookup-interface.js to handle interacting with the user Create event listener for the submit button to display the results of a user's search Link lookup.js and lookup-interface.js

---
+++ js/lookup-interface.js
@@ -0,0 +1,30 @@
+var Repos = require('./../js/lookup.js').reposModule;
+
+$(document).ready( function() {
+
+    var currentReposObject = new Repos();
+
+    // Debugger
+    console.log('I am batman');
+
+    $('#usernameForm').submit( function(event){
+        event.preventDefault();
+
+        // Debugger
+        console.log('I am groot');
+
+        var userInfo = $('#username').val();
+
+        // Clear form input field
+        $('#username').val("");
+
+        // Get user information
+        currentReposObject.getRepos();
+
+        // Debugger
+        console.log(userInfo);
+
+        // Display user information 
+        $('#showUserInfo').text(userInfo);
+    });
+});
