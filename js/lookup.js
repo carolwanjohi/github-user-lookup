99fe7eaa5b86a08caa264893ca65ef9d5869bbf8
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-09-08 11:37:48 +0300

    Create a parameter for getRepos Create parameter userInfo for getRepos function so as to get information on the user that has been searched

--- js/lookup.js
+++ js/lookup.js
@@ -3,14 +3,17 @@ var apiKey = require('./../.env').apiKey;
 
 function Repos() {}
 
-Repos.prototype.getRepos = function(){
-  // $.get('https://api.github.com/users/daneden?access_token=' 
-  //   + apiKey).then(function(response){
-  //       coonsole.log(response);
-  //   }).fail(function(error){
-  //       console.log(error.responseJSON.message);
-  //   });
-    console.log('Are you ready to look-up')
+Repos.prototype.getRepos = function(userInfo){
+    // Debugger  
+    console.log('Are you ready to look-up');
+
+  $.get('https://api.github.com/users/' + userInfo + 
+    '?access_token=' + apiKey
+    ).then(function(response){
+        console.log(response);
+    }).fail(function(error){
+        console.log(error.responseJSON.message);
+    });
 };
 
 exports.reposModule = Repos;
